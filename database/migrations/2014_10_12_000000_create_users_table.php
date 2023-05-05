07d057944deba0b9c590347789270f0a7c381995
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-05-06 02:06:32 +0300

    add column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('date_of_birth')->nullable();
+            $table->boolean('gender')->nullable()->comment('true: male - false:female');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
